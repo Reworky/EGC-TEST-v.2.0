4ac38d95beb7daa0c49dafb7f853b6358c5a015e
Олег Данилов <olegdanilov@MacBook-Air-Oleg.local>
2026-07-18 00:38:34 +0700

    Force dark theme regardless of device light/dark setting

--- miniapp/src/main.jsx
+++ miniapp/src/main.jsx
@@ -6,6 +6,8 @@ import App from './App.jsx'
 // Убрать белый/чёрный экран Telegram как можно раньше
 window.Telegram?.WebApp?.ready();
 window.Telegram?.WebApp?.expand();
+window.Telegram?.WebApp?.setBackgroundColor('#0f1020');
+window.Telegram?.WebApp?.setHeaderColor('#0f1020');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
